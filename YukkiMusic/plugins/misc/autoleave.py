820ae79bfc4863c89e850bb474472234883f17ed
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 10:29:58 +0700

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -8,13 +8,9 @@
 # All rights reserved.
 
 import asyncio
-from datetime import datetime
 
 import config
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki, autoend
-from YukkiMusic.utils.database import (get_client, is_active_chat,
-                                       is_autoend)
+from CilikMusic.utils.database import get_client, is_active_chat
 
 
 async def auto_leave():
@@ -37,9 +33,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001748410899
+                                and chat_id != -1001538752127
+                                and chat_id != -1001728281926
                             ):
                                 if not await is_active_chat(chat_id):
                                     try:
@@ -53,32 +49,3 @@ async def auto_leave():
 
 
 asyncio.create_task(auto_leave())
-
-
-async def auto_end():
-    while not await asyncio.sleep(5):
-        if not await is_autoend():
-            continue
-        for chat_id in autoend:
-            timer = autoend.get(chat_id)
-            if not timer:
-                continue
-            if datetime.now() > timer:
-                if not await is_active_chat(chat_id):
-                    autoend[chat_id] = {}
-                    continue
-                autoend[chat_id] = {}
-                try:
-                    await Yukki.stop_stream(chat_id)
-                except:
-                    continue
-                try:
-                    await app.send_message(
-                        chat_id,
-                        "Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat.",
-                    )
-                except:
-                    continue
-
-
-asyncio.create_task(auto_end())
