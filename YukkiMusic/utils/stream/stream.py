8b406a0ac0c24e32c1290734b79f2d84cbd48983
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 09:46:24 +0700

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -14,20 +14,20 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from YukkiMusic import Carbon, YouTube, app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import (add_active_chat,
+from AbingMusic import Carbon, YouTube, app
+from AbingMusic.core.call import Yukki
+from AbingMusic.misc import db
+from AbingMusic.utils.database import (add_active_chat,
                                        add_active_video_chat,
                                        is_active_chat,
                                        is_video_allowed, music_on)
-from YukkiMusic.utils.exceptions import AssistantErr
-from YukkiMusic.utils.inline.play import (stream_markup,
+from AbingMusic.utils.exceptions import AssistantErr
+from AbingMusic.utils.inline.play import (stream_markup,
                                           telegram_markup)
-from YukkiMusic.utils.inline.playlist import close_markup
-from YukkiMusic.utils.pastebin import Yukkibin
-from YukkiMusic.utils.stream.queue import put_queue, put_queue_index
-from YukkiMusic.utils.thumbnails import gen_thumb
+from AbingMusic.utils.inline.playlist import close_markup
+from AbingMusic.utils.pastebin import Yukkibin
+from AbingMusic.utils.stream.queue import put_queue, put_queue_index
+from AbingMusic.utils.thumbnails import gen_thumb
 
 
 async def stream(
