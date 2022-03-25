36c01b9b84bf17a5b42ff15b67c5d8058c3fa961
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 10:20:51 +0700

    Update live.py

--- YukkiMusic/plugins/play/live.py
+++ YukkiMusic/plugins/play/live.py
@@ -10,10 +10,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from YukkiMusic import YouTube, app
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.stream.stream import stream
+from AbingMusic import YouTube, app
+from AbingMusic.utils.channelplay import get_channeplayCB
+from AbingMusic.utils.decorators.language import languageCB
+from AbingMusic.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
