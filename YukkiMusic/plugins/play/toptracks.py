92336717c3c2a7cceb94435223ac4ccd8790c2d1
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 10:05:01 +0700

    Update toptracks.py

--- YukkiMusic/plugins/play/toptracks.py
+++ YukkiMusic/plugins/play/toptracks.py
@@ -13,14 +13,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from AbingMusic import app
+from AbingMusic.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from AbingMusic.utils.decorators.language import languageCB
+from AbingMusic.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from AbingMusic.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
