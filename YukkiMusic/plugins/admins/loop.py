02dbea9494d41e47581f3fe8b910927890044d6b
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 10:43:01 +0700

    Update loop.py

--- YukkiMusic/plugins/admins/loop.py
+++ YukkiMusic/plugins/admins/loop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database.memorydatabase import (get_loop,
+from AbingMusic import app
+from AbingMusic.utils.database.memorydatabase import (get_loop,
                                                       set_loop)
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from AbingMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 LOOP_COMMAND = get_command("LOOP_COMMAND")
