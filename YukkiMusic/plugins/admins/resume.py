856cec97caf41fb3d09a7a97d41b9ecdad5bcaac
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 10:41:46 +0700

    Update resume.py

--- YukkiMusic/plugins/admins/resume.py
+++ YukkiMusic/plugins/admins/resume.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_on
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from AbingMusic import app
+from AbingMusic.core.call import Yukki
+from AbingMusic.utils.database import is_music_playing, music_on
+from AbingMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 RESUME_COMMAND = get_command("RESUME_COMMAND")
