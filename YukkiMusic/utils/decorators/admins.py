4269f6cd3b9c9cc44dd0cf13364a9916e31b94a0
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 09:48:46 +0700

    Update admins.py

--- YukkiMusic/utils/decorators/admins.py
+++ YukkiMusic/utils/decorators/admins.py
@@ -11,9 +11,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import adminlist
 from strings import get_string
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_authuser_names, get_cmode,
+from AbingMusic import app
+from AbingMusic.misc import SUDOERS
+from AbingMusic.utils.database import (get_authuser_names, get_cmode,
                                        get_lang, is_active_chat,
                                        is_commanddelete_on,
                                        is_maintenance,
