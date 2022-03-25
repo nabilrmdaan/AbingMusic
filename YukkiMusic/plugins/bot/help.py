badb31f6c4790c1a9359eacf4c2ba2de29835488
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-25 10:36:32 +0700

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -15,13 +15,13 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import help_pannel
-from YukkiMusic.utils.database import get_lang, is_commanddelete_on
-from YukkiMusic.utils.decorators.language import (LanguageStart,
+from AbingMusic import app
+from AbingMusic.misc import SUDOERS
+from AbingMusic.utils import help_pannel
+from AbingMusic.utils.database import get_lang, is_commanddelete_on
+from AbingMusic.utils.decorators.language import (LanguageStart,
                                                   languageCB)
-from YukkiMusic.utils.inline.help import (help_back_markup,
+from AbingMusic.utils.inline.help import (help_back_markup,
                                           private_help_panel)
 
 ### Command
